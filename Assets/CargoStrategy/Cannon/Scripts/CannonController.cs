2f77e3065f195b160a76439872d23a23a6ef4bdb
Stuart <stuarta95@gmail.com>
2018-01-26 22:33:46 +0000

    Made some prefabs. Going to input manager

---
+++ Assets/CargoStrategy/Cannon/Scripts/CannonController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CargoStrategy.Cannon
+{
+    public class CannonController : MonoBehaviour
+    {
+
+
+
+
+        private void Update()
+        {
+
+        }
+
+
+        
+
+
+       
+    }
+}
