c1a6ad2abf2d601b1f8599b5f3e070f552001be7
Alex <alexandercarter95@gmail.com>
2018-01-27 03:26:52 +0000

    started doing some shit

---
+++ Assets/CargoStrategy/Terrain/Scripts/RoadMesh.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CargoStrategy.Terrain
+{
+
+    public class RoadMesh : MonoBehaviour {
+
+        [SerializeField]
+        private Vector3 start;
+
+        [SerializeField]
+        private Vector3 end;
+
+        [SerializeField]
+        private TerrainMesh m_mesh;
+        
+        public void Awake()
+        {
+            
+        }
+
+
+        public float GetDistanceToLineSegment(Vector3 position, Vector3 lineStart, Vector3 lineEnd)
+        {
+
+        }
+    }
+}
