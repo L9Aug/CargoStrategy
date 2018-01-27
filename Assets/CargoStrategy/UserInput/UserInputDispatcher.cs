c742e8ad7952ffc2ffbdc4d8f1024d849700e754
Stuart <stuarta95@gmail.com>
2018-01-27 01:14:22 +0000

    input management

--- Assets/CargoStrategy/UserInput/UserInputDispatcher.cs
+++ Assets/CargoStrategy/UserInput/UserInputDispatcher.cs
@@ -4,32 +4,79 @@ using UnityEngine;
 
 namespace CargoStrategy.UserInput
 {
+
     public class UserInputDispatcher : MonoBehaviour
     {
+        public event System.Action Player1FiringEvent;
+        public event System.Action Player2FiringEvent;
 
-        
+        private void Start()
+        {
+            Player1FiringEvent += delegate () { Debug.Log("Player 1 firing"); };
+            Player2FiringEvent += delegate () { Debug.Log("Player 2 firing"); };
+
+        }
+
         private void Update()
         {
-
-            Debug.Log(Input.GetAxis("P1Horizontal"));
-
-            /*
-            if (Input.GetAxis("P1Horizontal") > 0.5f)
+            if (Input.GetAxis("P1FireKey") > 0.5 || Input.GetAxis("P1Fire") > 0.5)
             {
-                Debug.Log("P1Horizontal: " + Input.GetAxis("P1Horizontal"));
+                System.Action temp = Player1FiringEvent;
+                if (temp != null)
+                {
+                    Player1FiringEvent();
+                }
             }
-            if (Input.GetAxis("P1Vertical") > 0.5f)
+            if (Input.GetAxis("P2FireKey") > 0.5 || Input.GetAxis("P2Fire") > 0.5)
             {
-                Debug.Log("P1Vertical: " + Input.GetAxis("P1Vertical"));
+                System.Action temp = Player1FiringEvent;
+                if (temp != null)
+                {
+                    Player2FiringEvent();
+                }
             }
-            if (Input.GetAxis("P1Fire") > 0.5f)
-            {
-                Input.ax
-                KeyCode.jo
-                Debug.Log("P1fire: " + Input.GetAxis("P1Fire"));
-            }
-
-    */
         }
+
+        public static float GetPlayer1HorizontalMovement()
+        {
+            return Input.GetAxis("P1Horizontal");
+        }
+        public static float GetPlayer1VerticalMovement()
+        {
+            return Input.GetAxis("P2Vertical");
+        }
+
+        public static float GetPlayer1HorizontalLook()
+        {
+            return Input.GetAxis("P1LookHorizontal");
+        }
+        public static float GetPlayer1VerticalLook()
+        {
+            return Input.GetAxis("P1LookVertical");
+        }
+
+        public static float GetPlayer2HorizontalMovement()
+        {
+            return Input.GetAxis("P2Horizontal");
+        }
+        public static float GetPlayer2VerticalMovement()
+        {
+            return Input.GetAxis("P2Vertical");
+        }
+
+        public static float GetPlayer2HorizontalLook()
+        {
+            return Input.GetAxis("P2LookHorizontal");
+        }
+        public static float GetPlayer2VerticalLook()
+        {
+            return Input.GetAxis("P2LookVertical");
+        }
+
+
+
+
+
+
     }
 }
