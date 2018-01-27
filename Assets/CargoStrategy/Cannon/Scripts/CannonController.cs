23c0a507a9887da407ab10dd8b58d1e918ad5699
Stuart <stuarta95@gmail.com>
2018-01-27 01:40:14 +0000

    updated cannon prefab

--- Assets/CargoStrategy/Cannon/Scripts/CannonController.cs
+++ Assets/CargoStrategy/Cannon/Scripts/CannonController.cs
@@ -1,19 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using CargoStrategy.UserInput;
 
 namespace CargoStrategy.Cannon
 {
     public class CannonController : MonoBehaviour
     {
+        public GameObject cannonBase;
+        public GameObject cannonGun;
 
 
-
-
+        
         private void Update()
         {
-
+            UserInputDispatcher.player
         }
 
 
