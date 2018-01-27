312d526cd5804b789ded1e1834ddd368df80120e
Alex <alexandercarter95@gmail.com>
2018-01-27 17:42:52 +0000

    road destruction

--- Assets/CargoStrategy/Cannon/Scripts/ProjectileController.cs
+++ Assets/CargoStrategy/Cannon/Scripts/ProjectileController.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CargoStrategy.Terrain;
 
 namespace CargoStrategy.Cannon
 {
     public class ProjectileController : MonoBehaviour
     {
+        [SerializeField]
+        private float m_blastRange = 5.0f;
+
         private const float ShotLifespan = 10;
 
         public GameObject CameraFocus;
@@ -53,9 +57,34 @@ namespace CargoStrategy.Cannon
         public void OnCollisionEnter(Collision collision)
         {
             Collided = true;
+            for (int i = 0; i < RoadMesh.RoadMeshes.Count; i++)
+            {
+                RoadMesh mesh = RoadMesh.RoadMeshes[i];
+                Vector3 closest = GetClosestPointOnLine(mesh.Start.position, mesh.End.position, transform.position);
+                if (Vector3.Distance(closest, transform.position) < m_blastRange)
+                {
+                    mesh.SetDestroy(true);
+                }
+            }
         }
 
-
+        private Vector3 GetClosestPointOnLine(Vector3 start, Vector3 end, Vector3 position)
+        {
+            Vector3 toStart = start - position;
+            Vector3 lineNormal = end - start;
+            if (Vector3.Dot(toStart, lineNormal) > 0.0f)
+            {
+                return start;
+            }
+            Vector3 toEnd = end - position;
+            if (Vector3.Dot(toEnd, lineNormal) < 0.0f)
+            {
+                return end;
+            }
+            position -= start;
+            return Vector3.Project(position, lineNormal) + start;
+        }
+        
     }
 
 }
