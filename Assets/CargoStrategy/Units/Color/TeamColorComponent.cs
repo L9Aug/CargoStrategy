ed36688b58bd14467906003f41c123e6b864c85d
Alex <alexandercarter95@gmail.com>
2018-01-28 00:25:07 +0000

    set up basic nodes in design scene

---
+++ Assets/CargoStrategy/Units/Color/TeamColorComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamColorComponent : MonoBehaviour {
+
+    [SerializeField]
+    private Renderer m_renderer = null;
+
+    [SerializeField]
+    private List<int> m_materialIds = new List<int>();
+
+    private List<Material> m_materialInstances = new List<Material>();
+
+    private void SetTeam(int team)
+    {
+
+    }
+
+
+}
